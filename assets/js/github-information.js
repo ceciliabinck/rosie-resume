a7632ea80e86afbb5f2d2de86447786436a3618c
ceciliabinck <cecilia4binck@gmail.com>
2020-03-24 16:02:25 +0000

    Fixed a bug I had with showing github usernames

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,22 +1,22 @@
 function userInformationHTML(user) {
     return `
-    <h2>${"user.name"}
+    <h2>${user.name}
         <span class="small-name">
-            (@<a href="${user.html_url}" target="_blank">${user.login}</a>
+            (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
         </span>
     </h2>
     <div class="gh-content">
         <div class="gh-avatar">
-            <a href="${user.html.url} target="_blank">
+            <a href="${user.html_url} target="_blank">
                 <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
             </a>
         </div>
-        <p>Followers: ${useer.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
+        <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
     </div>
     `
 }
 
-function fetchGitHubInformation(event){
+function fetchGitHubInformation(event) {
     let username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -28,18 +28,19 @@ function fetchGitHubInformation(event){
         </div>`);
     
     $.when(
-        $.getJSON(`http://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`)
     ).then (
         function(response) {
             let userData = response;
             $("#gh-user-data").html(userInformationHTML(userData));
-        }, function(errorResponse) {
+        }, 
+        function(errorResponse) {
             if (errorResponse.status === 404) {
-                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2> `);
-            }else {
+                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
-                    `<h2>Error; ${errorResponse.responseJSON.message}</h2>`);
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
                 }  
         });
 }       
